d6583794f6c8ab2d35979b80bc746a3d6f37ea5e
Albere <alberenorthey@gmail.com>
2025-05-03 12:36:34 +0100

    fix: adjust player size

--- game/Main.gd
+++ game/Main.gd
@@ -219,11 +219,11 @@ func _draw():
 		var p2 = (trail[i] + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE + Vector2(CELL_SIZE / 2, CELL_SIZE / 2)
 		draw_line(p1, p2, ORANGE, 2)
 
-	var player_rect = Rect2((player_pos + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE, Vector2(CELL_SIZE, CELL_SIZE))
+	var player_rect = Rect2((player_pos + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE, Vector2(17, 17))
 	draw_texture_rect(player_img, player_rect, false)
-	var heart_rect = Rect2(((end_1 + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE), Vector2(CELL_SIZE, CELL_SIZE))
+	var heart_rect = Rect2(((end_1 + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE), Vector2(17, 17))
 	draw_texture_rect(heart_img, heart_rect, false)
-	var brain_rect = Rect2(((end_2 + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE), Vector2(CELL_SIZE, CELL_SIZE))
+	var brain_rect = Rect2(((end_2 + Vector2(BORDER_WIDTH, BORDER_WIDTH)) * CELL_SIZE), Vector2(17, 17))
 	draw_texture_rect(brain_img, brain_rect, false)
 
 	# draw_texture_rect(player_img, player_rect, false)
